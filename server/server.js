e30b15f65eb3ebaf79446b32af38bec02d47d81b
taylan civaoglu <taylancivaoglu@gmail.com>
2018-12-21 14:02:01 +0300

    GET /todos

--- server/server.js
+++ server/server.js
@@ -23,6 +23,16 @@ app.post('/todos',(req, res)=>{
     });
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({
+            todos
+        });
+    }, (e) => {
+        res.status(200).send(e);
+    });
+});
+
 app.listen(3000, ()=>{
     console.log('Server started on port: 3000');
 });
